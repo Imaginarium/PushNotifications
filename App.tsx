18cd04347290e677316f2510304789700b552507
Miguel Miranda <miguel.a.m.mirand@gmail.com>
2023-02-27 12:38:48 +0000

    add loading

--- App.tsx
+++ App.tsx
@@ -1,13 +1,29 @@
-import * as React from "react";
+import React, { useState }from "react";
 import { WebView } from "react-native-webview";
+import { ActivityIndicator, View } from "react-native";
 import Keys from "./keys";
 
+export function MyWebview() {
+  const [loadingState, setLoadingState] = useState(true);
+
+  return (
+  <View style={{ flex: 1 }}>
+    <WebView 
+    onLoad={() => setLoadingState(false)}
+    source={{ uri: Keys.URL }} 
+    />
+    {loadingState && (
+        <ActivityIndicator
+          style={{ position: "absolute", top: 0, left: 0, bottom: 0, right: 0 }}
+          size="large"
+        />
+      )}
+  </View>
+  );
+}
 export default class App extends React.Component {
+  
   render() {
-    return (
-      <WebView 
-      source={{ uri: Keys.URL }} 
-      />
-    );
+    return <MyWebview></MyWebview>
   }
 }
